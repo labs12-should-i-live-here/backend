90e5e5c9e811b48ae414b60c8fbc3e988b1f34f8
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-24 18:09:44 -0400

    added middleware helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const middleware = require('./middleware/serverMiddleware')
+
 const server = express();
+
+middleware(server);
 
 //function that displays current date
 const utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
