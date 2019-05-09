3e11219b63e2e92d136093b79cf980ff8b134267
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-09 15:48:22 -0400

    fixed secret error

--- api/data/extendedConfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtSecret:
-    process.env.JWT_SECRET || "Hic Rhodus! This is the place to jump, the place to dance! This is the wilderness! Was there ever any other? This is savagery! Do you call it freedom? This is barbarism! The struggle for survival is right here. Haven’t we always known it? Isn’t this a public secret? Hasn’t it always been the big public secret? ",
-};
--- api/middleware/gentoken.js
+++ api/middleware/gentoken.js
@@ -1,18 +1,23 @@
+const ex = require('../data/extendedConfig');
+
 module.exports = {
     generateToken 
 };
 
 const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET;
+const secret = process.env.SECRET || 'string';
 
     
     function generateToken ( user ) {
+
         const payload = {
-                        subject: user.id,
-                        username: user.username,
+                        subject: user.userid,
+                        username: "taylor"
                     }
     const options = {
                         expiresIn: '78h'
                     }
+                    console.log( payload, options, secret)
+                    try {console.log(jwt.sign( payload, secret, options), "recived")}catch(err){console.log(err)}
     return jwt.sign( payload, secret, options);
     }
